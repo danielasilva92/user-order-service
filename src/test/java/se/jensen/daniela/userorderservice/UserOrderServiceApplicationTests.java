9c378ea32cbda614d14649cff766a498fa7f9e1d
Daniela Silva <daniela92_@hotmail.de>
2026-05-29 14:49:59 +0200

    removed context testing for now

--- src/test/java/se/jensen/daniela/userorderservice/UserOrderServiceApplicationTests.java
+++ src/test/java/se/jensen/daniela/userorderservice/UserOrderServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package se.jensen.daniela.userorderservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserOrderServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
